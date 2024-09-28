a5fffee64db6e9b87b9b2359dd1f26f048b56bf3
Haoz-Rundrego <fanathl6@gmail.com>
2024-09-28 16:29:59 +0300

    add dinamics list v0.1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const ad = this.document.querySelectorAll(".p2_second_parts_frame");
+  const home_btn = this.document.querySelector(".home");
 
+  ad.forEach(function (ad) {
+    ad.addEventListener("click", function () {
+      const adId = ad.getAtribute("date-id");
+      window.location.href = `details.html?id=${adId}`;
+    });
+  });
+  home_btn.addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
+});
